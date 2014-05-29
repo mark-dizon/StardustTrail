78a7cf30d30776ec5b4f18931eff11524dc776c4
rghassem <rghassem@gmail.com>
2014-05-28 23:33:41 -0700

    Added logic for playing through graph (visuals only)

--- jobNode.js
+++ jobNode.js
@@ -1,96 +1,146 @@
+
+//Represents one graphical node in the job graph
+function JobNode(options) {
+
+	var distance_between_nodes = 400;
 	
-	//Represents one graphical edge in the job graph
-	function JobNodeEdge(parent, start, end) {
+	var state = options.state;
+	var x = options.x;
+	var y = options.y;
+	
+	var graphics = state.add.graphics(x,y);
 
-		var graphics = new Phaser.Graphics(0, 0);
-		parent.addChild(graphics);
+	var edgeData = [];
 
-		function draw(progress){
-			graphics.lineStyle(2, 0xFCAE1C);
+	//Public members
+	graphics.upperEdge = function(){ 
+		return {x: graphics.x, y: graphics.y};
+	};
+
+	graphics.lowerEdge = function(){ 
+		return {x: graphics.x, y: graphics.y};
+	};
+	graphics.leftEdge = function(){ 
+		return {x: graphics.x, y: graphics.y}; 
+	};
+	graphics.rightEdge = function(){ 
+		return {x: graphics.x, y: graphics.y};
+	};
+
+	graphics.state = state; 
+
+
+	//Methods
+
+	//Connects a node to this one. Optional third parameter is the class of Edge to use to make the connection
+	graphics.addConnectedNode = function(otherNode, direction, edge) {
+		edge = edge || JobNodeEdge(state);
+
+		function getNodeEdge(node, direction) {
+			switch(direction) {
+				case directions.left  : return node.leftEdge();
+				case directions.right : return node.rightEdge();
+				case directions.up    : return node.upperEdge();
+				case directions.down  : return node.lowerEdge();
+			}
+		}
+
+		function setConnectedNodePosition(node, direction){
+			node.x = 0;
+			node.y = 0;
+			switch(direction) {
+				case directions.left  : return node.x = -distance_between_nodes;
+				case directions.right : return node.x = distance_between_nodes;
+				case directions.up    : return node.y = -distance_between_nodes;
+				case directions.down  : return node.y = distance_between_nodes;
+			}
+		}
+
+		//make the other node anchored relative to this one
+		graphics.addChild(otherNode);
+		setConnectedNodePosition(otherNode, direction);
+
+		//draw a line between them
+		var start = getNodeEdge(graphics, direction);
+		var end = getNodeEdge(otherNode, -direction);
+		edge.initEdge(graphics, start, end);
+		edgeData.push({edge: edge, node: otherNode});
+
+		return otherNode;
+	}
+
+	graphics.animateToNext = function(edgeIndex){
+		var edge = edgeData[edgeIndex].edge;
+		edge.playTraceAnimation(10);
+	}
+
+	graphics.redraw = draw;
+
+	graphics.update = function() {
+		graphics.children.forEach(function(child){
+			child.update();
+		});
+	}
+
+	function draw() { 
+		graphics.clear(); //Stub. Real implemenation in subclass
+	}
+
+	return graphics;
+
+}
+
+
+//Represents one graphical edge in the job graph.
+//Has playTraceAnimation function to animate a change in color from start to end.
+function JobNodeEdge(state, color, completeColor) {
+	color = color || 0xFCAE1C;
+	completeColor = completeColor || 0xFFFFFF;
+
+	var graphics = state.add.graphics(0,0);
+	var start = {x:0, y:0};
+	var end = {x:0, y:0};
+
+	function draw(progress){
+		graphics.clear();
+		var nonProgressStart = start;
+		if(progress > 0){
+			graphics.lineStyle(3, completeColor);
 			graphics.moveTo(start.x, start.y);
-			graphics.lineTo(end.x, end.y);
+			var progressEnd = {
+				x: start.x + (end.x - start.x) * progress,
+				y: start.y + (end.y - start.y) * progress
+			};
+			graphics.lineTo(progressEnd.x, progressEnd.y);
+			nonProgressStart = progressEnd;
 		}
 
-		graphics.redraw = draw;
-
-		draw(0); //draw right away
-		return graphics;
+		graphics.lineStyle(2, color);
+		graphics.moveTo(nonProgressStart.x, nonProgressStart.y);
+		graphics.lineTo(end.x, end.y);
 	}
 
-	//Represents one graphical node in the job graph
-	function JobNode(options) {
+	graphics.redraw = draw;
 
-		var distance_between_nodes = 400;
-		
-		var state = options.state;
-		var x = options.x;
-		var y = options.y;
-		
-		var graphics = state.add.graphics(x,y);
-
-		var edges = [];
-
-		//Public members
-		graphics.upperEdge = function(){ 
-			return {x: graphics.x, y: graphics.y};
-		};
-
-		graphics.lowerEdge = function(){ 
-			return {x: graphics.x, y: graphics.y};
-		};
-		graphics.leftEdge = function(){ 
-			return {x: graphics.x, y: graphics.y}; 
-		};
-		graphics.rightEdge = function(){ 
-			return {x: graphics.x, y: graphics.y};
-		};
-
-
-		//Methods
-
-		//Connects a node to this one. Use graphics.lineStyle to setup the the appearance of connecting lines
-		graphics.addConnectedNode = function(otherNode, direction) {
-
-			function getNodeEdge(node, direction) {
-				switch(direction) {
-					case directions.left  : return node.leftEdge();
-					case directions.right : return node.rightEdge();
-					case directions.up    : return node.upperEdge();
-					case directions.down  : return node.lowerEdge();
-				}
-			}
-
-			function setConnectedNodePosition(node, direction){
-				node.x = 0;
-				node.y = 0;
-				switch(direction) {
-					case directions.left  : return node.x = -distance_between_nodes;
-					case directions.right : return node.x = distance_between_nodes;
-					case directions.up    : return node.y = -distance_between_nodes;
-					case directions.down  : return node.y = distance_between_nodes;
-				}
-			}
-
-			//make the other node anchored relative to this one
-			graphics.addChild(otherNode);
-			setConnectedNodePosition(otherNode, direction);
-
-			//draw a line between them
-			var start = getNodeEdge(graphics, direction);
-			var end = getNodeEdge(otherNode, -direction);
-			var edge =  new JobNodeEdge(graphics, start, end);
-			edges.push(edge);
-
-			return otherNode;
-		}
-
-		graphics.animateToNext = function(){}
-
-		graphics.redraw = draw;
-
-		function draw() {
-		}
-
-		return graphics;
-
+	graphics.initEdge = function(parent, startIn, endIn) {
+		start = startIn;
+		end = endIn;
+		parent.addChild(graphics);
+		draw();
 	}
+
+	graphics.playTraceAnimation = function(duration, callback) {
+		var progress = 0;
+		graphics.update = function() {
+			progress += (game.time.elapsed/1000 )/duration ; //get frame delta time here
+			if(progress >= 1) {
+				progress = 1
+				graphics.update = function(){};
+				if(callback) callback();
+			}
+			draw(progress);
+		}
+	}
+
+	return graphics;
+}
--- jobView.js
+++ jobView.js
@@ -17,6 +17,7 @@ function JobView() {
 	}
 
 	function create(){
+		//Mockup job graph
 		node = new TaskNode( {x: 10, y:10, state: state} );
 		node.setSuccessNode(new TaskNode({state: state}))
 			.setSuccessNode(new TaskNode({state: state}))
@@ -27,7 +28,19 @@ function JobView() {
 	}
 
 	function handleInput(event) {
-		game.changeState(states.map);
+		if(event.keyCode == Phaser.Keyboard.SPACEBAR) {
+			//Fake resolving the mockup job graph
+			node.next(true, function(nextNode){
+				nextNode.next(true, function(nextNode){
+					nextNode.next(false, function(nextNode){
+						nextNode.next();
+					});
+				});
+			});
+		}
+		else {
+			game.changeState(states.map);
+		}
 	}
 
 	function initJob(job) {
