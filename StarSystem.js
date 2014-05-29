594a1b6bb9f979fd12763745d8b13cab41365bae
Mark Dizon <mark.dizon@makerstudios.com>
2014-05-28 21:38:35 -0700

    Map movement and zoom

--- StarSystem.js
+++ StarSystem.js
@@ -1,4 +1,4 @@
-function StarSystem() {
+function StarSystem(state) {
 	var currentPlanet = 0;
 	var numPlanets= 9;
 	var planets = [
@@ -14,7 +14,14 @@ function StarSystem() {
 			y : 300,
 			rad : 100,
 			color: 0x330033
-		}
+		},
+
+		{
+			x : 0,
+			y : 0,
+			rad : 100,
+			color: 0x335533
+		},
 	];
 	var edges = [];
 
@@ -25,10 +32,8 @@ function StarSystem() {
 
 	function drawMap(graphics) {
 		graphics.beginFill(0xFFFFFF);
-		for(var i = 0; i < 300; i++) {
-			var randomX = Math.floor(Math.random() * screenWidth);
-			var randomY = Math.floor(Math.random() * screenHeight);
-			graphics.drawCircle(randomX, randomY, 1);
+		for(var i = 0; i < 2000; i++) {
+			graphics.drawCircle(state.world.randomX, state.world.randomY, 1);
 		}
 		planets.forEach(function(planet) {
 			graphics.beginFill(planet.color);
